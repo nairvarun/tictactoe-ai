1d46021012cffcc9f1ab1566aee73715ea3b92aa
varun <10447555-nairvarun@users.noreply.gitlab.com>
2022-01-28 18:05:07 +0530

    bot.c working

--- bot.c
+++ bot.c
@@ -1,94 +1,242 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
-char * GameInit(void);
-void PrintBoard(char board[]);
-int * GameOver(void);
-void StartGme(void);
+int *GameInit(void);
+void PrintBoard(int board[]);
+int GetGameStatus(int board[]);
+int *GetValidMoves(int board[]);
+void StarGame(int board[]);
+void Play(int board[], int is_ai_move);
+int Minimax(int board[], int is_maximizing, int alpha, int beta);
 
 int main(void)
 {
-	char *board;
-	int *GameStatus;
+	int *board;
 	board = GameInit();
-	GameStatus = GameOver();
-	for (int i = 0; i<2; i++)
-	{
-		printf("%d\n", GameStatus[i]);
-	}
+	PrintBoard(board);
+	StarGame(board);
 	return 0;
 }
 
-char * GameInit(void)
+int *GameInit(void)
 {
-	char *board = malloc(sizeof(*board) * 11);
+	// board[0] ==> status of game wrt x
+		// 0 ==> ai playing as o
+		// 1 ==> ai playing as x
+	// return vals:
+		// 0 ==> draw
+		// 1 ==> x (ai) won
+		// -1 == x (human) won
+	int *board = calloc(10, sizeof(*board));
+	for (int i = 1; i < 10; i++)
+	{
+		board[i] = i;
+	}
+	
 	while (1)
 	{
-		printf("do you want to play as 'x' or 'o'? ");
-		board[0] = getchar();	/* board[0] represents if the human is playing as 'x' or 'o' */
-
-		// clear input stream
-		char c;
-		while ((c = getchar()) != '\n' && c != EOF) {}
-		
-		if (board[0] == 'x' || board[0] == 'o' || board[0] == 'X' || board[0] == 'O')
+		printf("what do you wnat to play as? [x/o]\n> ");
+		char choice = getchar();
+		if (choice == 'x')	// ai ==> o 
 		{
+			board[0] = 'o' - '0';
+			int _; while ((_ = getchar()) != '\n' && _ != EOF) {}	/* clear input stream */
+			break;
+		}
+		else if (choice == 'o')	// ai ==> x
+		{
+			board[0] = 'x' - '0';
+			int _; while ((_ = getchar()) != '\n' && _ != EOF) {}	/* clear input stream */
 			break;
 		}
 		else
 		{
-			printf("invalid choice\n\n");
+			printf("\ninvalid choice! ");
+			int _; while ((_ = getchar()) != '\n' && _ != EOF) {}	/* clear input stream */
 		}
 	}
-	
-	printf("\nyou will be playing as '%c'\n\n", board[0]);
-	for (int i = 1; i < 10; i++)
-	{
-		board[i] = i + '0';
-	}
-	board[0] = 's';
-	PrintBoard(board);
 
 	return(board);
 }
 
-void PrintBoard(char board[])
+void PrintBoard(int board[])
 {
-	// todo: print in the middle of the terminal screen 
-	// clears treminal screen
-	// printf("%c[2J%c[;H", (char) 27, (char) 27);
-	
-	for (int i = 1; i < 10; i += 3)
+	printf("\n");
+	for (int i = 1;  i < 10; i += 3)
 	{
-		printf("|%c|%c|%c|\n", board[i], board[i+1], board[i+2]);
-		// only print divisions after the first and second line
-		if (i < 7)
+		printf("|%c|%c|%c|\n", board[i] + '0', board[i+1] + '0', board[i+2] + '0');
+		if (i < 7) {printf("--+-+--\n");}
+	}
+	printf("\n");
+}
+
+int GetGameStatus(int board[])
+{
+	int *valid_moves;
+	valid_moves = GetValidMoves(board);
+
+	// board[0] ==> status of game wrt x
+		// 0 ==> ai playing as o
+		// 1 ==> ai playing as x
+	// return vals:
+		// 0 ==> draw
+		// 1 ==> x (ai) won
+		// -1 ==> x (human) won
+		// 100 ==> game not over
+	if (board[1] == board[2] && board[2] == board[3]){return (board[0] == board[1] ? 1 : -1);}
+	else if (board[4] == board[5] && board[5] == board[6]){return (board[0] == board[4] ? 1 : -1);}
+	else if (board[7] == board[8] && board[8] == board[9]){return (board[0] == board[7] ? 1 : -1);}
+	else if (board[1] == board[4] && board[4] == board[7]){return (board[0] == board[1] ? 1 : -1);}
+	else if (board[2] == board[5] && board[5] == board[8]){return (board[0] == board[2] ? 1 : -1);}
+	else if (board[7] == board[6] && board[6] == board[9]){return (board[0] == board[7] ? 1 : -1);}
+	else if (board[1] == board[5] && board[5] == board[9]){return (board[0] == board[1] ? 1 : -1);}
+	else if (board[3] == board[5] && board[5] == board[7]){return (board[0] == board[3] ? 1 : -1);}
+	else if (valid_moves[0] == 0) {return 0;}
+	else {return(100);}
+}
+
+int *GetValidMoves(int board[])
+{
+	int *valid_moves = calloc(9, sizeof(*valid_moves));
+	int valid_move_counter = 0;
+	for (int i = 1; i < 10; i ++)
+	{
+		if (board[i] <= 9)
 		{
-			printf("-------\n");
+			valid_moves[valid_move_counter] = i;
+			valid_move_counter++;
 		}
 	}
+	return (valid_moves);
 }
 
-void StartGme(void)
+void StarGame(int board[])
 {
-	
+	int is_ai_move = (board[0] == 'x' - '0') ? 1 : 0;
+
+	while(GetGameStatus(board) == 100)
+	{
+		Play(board, is_ai_move);
+		is_ai_move = is_ai_move ? 0 : 1;
+	}
 }
 
-int * GameOver(void)
+void Play(int board[], int is_ai_move)
 {
-	int *GameStatus = calloc(2, sizeof(*GameStatus));
+	if (is_ai_move)
+	{
+		int *valid_moves, score, best_score, best_move;
+		best_score = INT_MIN;
+		valid_moves = GetValidMoves(board);
 
-	// [0=game not over;1=game over, 0=draw, 1=ai won, -1=human won]
-	if (board[1] == board[2] && board[2] == board[3])
-	{
-		GameStatus[0] = 1;
-		GameStatus[1] = board[0] == board[1] ? -1 : 1;
+		for (int i = 0; i < 9; i++)
+		{
+			if (valid_moves[i])
+			{
+				board[valid_moves[i]] = board[0];
+				score = Minimax(board, 0, INT_MIN, INT_MAX);
+				board[valid_moves[i]] = valid_moves[i];
+				if (score > best_score)
+				{
+					best_score = score;
+					best_move = valid_moves[i];
+				}
+			}
+		}
+		board[best_move] = board[0];
+		printf("\nai played at %d\n", best_move);
+		PrintBoard(board);
 	}
-	else if (board[4] == board[5] && board[5] == board[6])
+	else
 	{
-		GameStatus[0] = 1;
-		GameStatus[1] = board[0] == board[1] ? -1 : 1;
+		int *valid_moves;
+		valid_moves = GetValidMoves(board);
+		while (1)
+		{
+			printf("your move?\n> ");
+			char choice = getchar() - '0';
+			for (int i = 0; i < 9; i++)
+			{
+				// printf("%d, %d\n", valid_moves[i], choice);
+				if (valid_moves[i] == choice)
+				{
+					board[valid_moves[i]] = (board[0] == 'x' - '0') ? ('o' - '0') : ('x' - '0');
+					int _; while ((_ = getchar()) != '\n' && _ != EOF) {}	/* clear input stream */
+					goto end;
+				}
+			}
+			printf("\ninvalid choice! ");
+			int _; while ((_ = getchar()) != '\n' && _ != EOF) {}	/* clear input stream */
+		}
+		end:
+		PrintBoard(board);
 	}
-	
-	return(GameStatus);
+}
+
+int Minimax(int board[], int is_maximizing, int alpha, int beta)
+{
+	if (GetGameStatus(board) != 100)
+	{
+		return GetGameStatus(board);
+	}
+	else
+	{
+		if (is_maximizing)
+		{
+			int *valid_moves, score, best_score, best_move;
+			best_score = INT_MIN;
+			valid_moves = GetValidMoves(board);
+			for (int i = 0; i < 9; i++)
+			{
+				if (valid_moves[i])
+				{
+					board[valid_moves[i]] = board[0];
+					score = Minimax(board, 0, INT_MIN, INT_MAX);
+					board[valid_moves[i]] = valid_moves[i];
+					if (score > best_score)
+					{
+						best_score = score;
+					}
+					if (best_score > alpha)
+					{
+						alpha = best_score;
+					}
+					if (alpha >= beta)
+					{
+						break;
+					}
+				}
+			}
+			return best_score;
+		}
+		else
+		{
+			int *valid_moves, score, best_score, best_move;
+			best_score = INT_MAX;
+			valid_moves = GetValidMoves(board);
+			for (int i = 0; i < 9; i++)
+			{
+				if (valid_moves[i])
+				{
+					board[valid_moves[i]] = (board[0] == 'x' - '0') ? ('o' - '0') : ('x' - '0');
+					score = Minimax(board, 1, INT_MIN, INT_MAX);
+					board[valid_moves[i]] = valid_moves[i];
+					if (score < best_score)
+					{
+						best_score = score;
+					}
+					if (best_score < beta)
+					{
+						beta = best_score;
+					}
+					if (alpha >= beta)
+					{
+						break;
+					}
+				}
+			}
+			return best_score;
+		}
+	}
 }
